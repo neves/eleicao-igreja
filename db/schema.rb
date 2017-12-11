62db04f0a95723291d9a2209a1e69262a13c7c62
Marcos Neves <marcos.neves@gmail.com>
2017-12-11 11:02:06 -0200

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_12_11_115735) do
+ActiveRecord::Schema.define(version: 2017_12_11_130113) do
 
   create_table "roles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.integer "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
 end
