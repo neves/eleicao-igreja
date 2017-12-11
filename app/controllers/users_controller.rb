4ba558fcb1455bf70e52c7bf90576962e03ef3cb
Marcos Neves <marcos.neves@gmail.com>
2017-12-11 11:21:42 -0200

    Use password field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :name, :password_digest, :role_id)
+      params.require(:user).permit(:email, :name, :password, :role_id)
     end
 end
